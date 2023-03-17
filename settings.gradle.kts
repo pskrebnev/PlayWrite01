ef0e894fa819c93e56ed6acee7f59483896ba5ee
pskrebnev <Kumbaya33>
2023-03-17 21:53:18 +0100

    Init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "PlayWrite01"
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch();
+            Page page = browser.newPage();
+            page.navigate("http://playwright.dev");
+            System.out.println("The page title is: <" + page.title() + ">");
+        }
+    }
+}
