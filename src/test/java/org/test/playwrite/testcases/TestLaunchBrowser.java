cc2f1ed68545738a0e207a35cc533c5b5623219b
pskrebnev <Kumbaya33>
2023-03-19 14:42:30 +0100

    Maximizing window

--- src/test/java/org/test/playwrite/testcases/TestLaunchBrowser.java
+++ src/test/java/org/test/playwrite/testcases/TestLaunchBrowser.java
@@ -1,6 +1,7 @@
 package org.test.playwrite.testcases;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
@@ -9,19 +10,29 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class TestLaunchBrowser {
 
     public Playwright pw;
     public Browser browser;
+    public Page page;
 
     private String url = "https://applitools.com/blog/top-playwright-questions-answered/";
 
     @BeforeClass
     public void prepare() {
         pw = Playwright.create();
-        browser = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
+        // through properties
+        ArrayList<String> arguments = new ArrayList<>();
+        arguments.add("--start-maximized");
+        browser = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(arguments));
+
+        BrowserContext bc = browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
+        page = bc.newPage();
+
+        // through dimension
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double width = screenSize.getWidth();
         double height = screenSize.getHeight();
@@ -31,7 +42,6 @@ public class TestLaunchBrowser {
 
     @Test
     public void testLaunchBrowser() {
-        Page page = browser.newPage();
         page.navigate(url);
         System.out.println("The title is: <" + page.title() + ">");
     }
