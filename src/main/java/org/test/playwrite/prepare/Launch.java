5b63d467ede58e786cbbf8b2bbed09f9ef7fff3f
pskrebnev <Kumbaya33>
2023-03-19 21:21:33 +0100

    Moved common methods to separate class

--- src/main/java/org/test/playwrite/prepare/Launch.java
+++ src/main/java/org/test/playwrite/prepare/Launch.java
@@ -1,4 +1,30 @@
 package org.test.playwrite.prepare;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.util.ArrayList;
+
 public class Launch {
+
+    protected Playwright pw;
+    protected Browser browser;
+    protected Page page;
+
+    public void prepare() {
+        pw = Playwright.create();
+        ArrayList<String> properties = new ArrayList<>();
+        properties.add("--start-maximized");
+        browser = pw.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(properties));
+        BrowserContext bc = browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
+        page = bc.newPage();
+    }
+
+    public void tierDown() {
+        page.close();
+        pw.close();
+    }
 }
