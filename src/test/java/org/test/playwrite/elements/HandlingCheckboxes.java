4ff951b8b4e8833d785729eab42f829b31558ae6
pskrebnev <Kumbaya33>
2023-05-07 01:00:28 +0200

    Check checkboxes

---
+++ src/test/java/org/test/playwrite/elements/HandlingCheckboxes.java
@@ -0,0 +1,35 @@
+package org.test.playwrite.elements;
+
+import com.microsoft.playwright.Locator;
+import org.test.playwrite.prepare.Launch;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HandlingCheckboxes extends Launch {
+
+    @BeforeClass
+    public void set() {
+        prepare();
+    }
+
+    @Test
+    public void testCheckboxes() throws InterruptedException {
+        String URL_PAGE1 = "http://www.tizag.com/htmlT/htmlcheckboxes.php";
+        Locator setCheckboxes = page.locator("//h2[text() = 'HTML Checkbox Form:']/" +
+                "following-sibling::div[1]/.//input[@type='checkbox']");
+
+        page.navigate(URL_PAGE1);
+        Thread.sleep(60_000);
+
+        for (int i = 0; i < setCheckboxes.count(); i++) {
+            setCheckboxes.nth(i).click();
+        }
+    }
+
+    @AfterClass
+    public void down() throws InterruptedException {
+        tierDown();
+        Thread.sleep(5000);
+    }
+}
--- src/test/java/org/test/playwrite/elements/Page01.java
+++ src/test/java/org/test/playwrite/elements/Page01.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Page01 extends Launch {
-    private String page1 = "https://www.google.com/";
+    private String page1 = "https://edition.cnn.com/";
 
     @BeforeClass
     public void set() {
@@ -14,9 +14,12 @@ public class Page01 extends Launch {
     }
 
     @Test
-    public void testPage() {
+    public void testPage() throws InterruptedException {
         page.navigate(page1);
         System.out.println("Page title is <" + page.title() + ">");
+//        page.locator("//input [@name = 'q']").type("Oshkosh");
+//        page.keyboard().press("F");
+        Thread.sleep(5000);
     }
 
     @AfterClass
